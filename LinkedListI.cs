d3b1ea4a167a2584411a71ebb080ba0b23a4fb19
yashwanthgh <yugnasura@gmail.com>
2024-04-02 11:06:47 +0530

    Added: Functionality of Adding element to the Linked List

---
+++ LinkedListI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAImplementation
+{
+    public class LinkedListI
+    {
+        private Node? head;
+
+        public void Add(int data)
+        {
+            Node node = new()
+            {
+                Data = data
+            };
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node current = head;
+                while(current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = node;
+            }
+           
+        }
+
+        public void Print()
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.Write(node.Data+" ");
+                node = node.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            LinkedListI instance = new LinkedListI();
+            instance.Add(56);
+            instance.Add(30);
+            instance.Add(70);
+            instance.Print();   
+        }
+    }
+}
